ccf818637fd93513d2f41a18b407a6385fa4958f
Daisyyyyy0 <soniahsiao16@gmail.com>
2023-12-30 17:55:36 +0800

    build: route & controller setup

--- app.js
+++ app.js
@@ -3,9 +3,20 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-      res.send('express app for movies')
+    //   res.send('express app for movies')
+    res.redirect('/movies')
 })
 
+app.get('/movies', (req, res) => {
+    res.send('Listening movies')
+})
+app.get('/movies/:id', (req, res) => {
+    const params = req.params
+    const id = req.params.id
+    console.log('params',params)
+    console.log('params.id',params.id)
+    res.send(`read movie:${id}, params:${params}`)
+})
 app.listen(port, () => {
       console.log(`express server is running on http://localhost:${port}`)
 })
